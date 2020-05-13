450044dbf9b263d96e14d5be5b9a253aed6d1f94
neeraj2296 <neerajkumarsj96@gmail.com>
2020-05-13 11:58:46 +0530

    Update wines_KClust.R

--- wines_KClust.R
+++ wines_KClust.R
@@ -2,19 +2,18 @@
 library(plyr)
 library(animation)
 wine <- read.csv(file.choose())
-
+#Importing and Editing the Data for the algorithm
 View(wine)
 wdata<-wine[,-1]
 attach(wdata)
 cor(wdata)
 pcaobj1<-princomp(wdata, cor = TRUE, scores = TRUE, covmat = NULL)
 
+#Visualising the data set after passing through the algorithm
 str(pcaobj1)
 summary(pcaobj1)
 loadings(pcaobj1)
-
 plot(pcaobj1)
-
 biplot(pcaobj1)
 pcaobj1
 # cbind used to bind the data in column wise
@@ -31,7 +30,7 @@ final2<- data.frame(wdata, fit$cluster) # append cluster membership
 final2
 final3 <- final2[,c(ncol(final2),1:(ncol(final2)-1))]
 aggregate(wdata[,1:16], by=list(fit$cluster), FUN=mean)
-
+# preparing data for clustering (considering only pca scores as they represent the entire data)
 twss = c()
 for (i in 2:15) twss[i] = sum(kmeans(norm_data, centers=i)$withinss)
 plot(1:15, twss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")   # Look for an "elbow" in the scree plot #
@@ -48,6 +47,6 @@ library(cluster)
 xds <- rbind(cbind(rnorm(wdata), rnorm(wdata)), cbind(rnorm(wdata), rnorm(wdata)))
 xcl <- clara(xds, 3, sample = 100)
 clusplot(xcl)
-
+#Saving the output to a new file
 write.csv(final3,file="WinesPCA_FINAL_kClust.csv",row.names = F, col.names = F)
 getwd()
